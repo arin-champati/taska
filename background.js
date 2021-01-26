ffde00a99d4470e7f7c5868fdcb6777966c7473e
Ethan Seide <eseide@dynamic-oit-vapornet100-10-9-100-250.princeton.edu>
2021-01-26 17:00:25 -0500

    Background content almost working

--- background.js
+++ background.js
@@ -1,17 +1,28 @@
 //
 // Block sites
+
 function handleTabChange(activeInfo) {
     chrome.tabs.query({'active':true,'currentWindow':true},function(array_of_tabs){//Gives details of the active tab in the current window.
         // alert(array_of_tabs[0].url);
         // get list of blocked sites from storage
-        let currentUrl = array_of_tabs[0].url
-        chrome.storage.sync.get(['blockList'], function(blockList) {
-            // console.log('Value currently is ' + result.key);
-            
-          });
-        // if url in blocked sites list, pass message to content (for blocking)
-        if(blockList.includes(currentUrl))
-        chrome.tabs.sendMessage(activeTab.id, {"blockLink": currentUrl});
-})}
+        let currentUrl = array_of_tabs[0].url;
+        let blockList = ['youtube.com'];
+        // chrome.storage.sync.get(['blockList'], function(blockList) {
+            for (let i = 0; i < blockList.length; i++) {
+                // if url in blocked sites list, pass message to content (for blocking)
+                // alert ("Blocked: " + blockList[i]);
+                if (currentUrl.includes(blockList[i])) {
+                    // alert("Sending message");
+                    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                        var activeTab = tabs[0];
+                        chrome.tabs.sendMessage(activeTab.id, {"blockLink": currentUrl});
+                });
+            }
+        }
+        // });
+    });
+        
+};
 
 chrome.tabs.onActivated.addListener(handleTabChange);
+chrome.tabs.onUpdated.addListener(handleTabChange);
--- content.js
+++ content.js
@@ -1,8 +1,12 @@
+console.log("CONTENT LOADED");
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+      alert("RECEIVED MESSAGE");
+      alert(request);
       // block webpage content
         if(request.blockLink) {
-            // block content
+            document.body.innerHTML = "";
         }
     }
   );
