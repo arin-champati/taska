703b1f6b353c71181d369b50221bd9fd61ef8c89
Jackson Deitelzweig <jackson.deitelzweig@gmail.com>
2021-01-26 19:28:11 -0500

    Blocking functionality

--- background.js
+++ background.js
@@ -1,28 +1,41 @@
-//
-// Block sites
+// Initialize block list
+chrome.storage.sync.set({'blockList': []}, function() {
+  console.log('Initialize block list');
+});
 
+
+// Block sites
 function handleTabChange(activeInfo) {
-    chrome.tabs.query({'active':true,'currentWindow':true},function(array_of_tabs){//Gives details of the active tab in the current window.
-        // alert(array_of_tabs[0].url);
+    // Gives details of the active tab in the current window.
+    chrome.tabs.query({'active':true,'currentWindow':true},function(array_of_tabs){
         // get list of blocked sites from storage
         let currentUrl = array_of_tabs[0].url;
-        let blockList = ['youtube.com'];
-        // chrome.storage.sync.get(['blockList'], function(blockList) {
+        chrome.storage.sync.get(['blockList'], function(result) {
+            let blockList = result.blockList;
             for (let i = 0; i < blockList.length; i++) {
                 // if url in blocked sites list, pass message to content (for blocking)
-                // alert ("Blocked: " + blockList[i]);
                 if (currentUrl.includes(blockList[i])) {
-                    // alert("Sending message");
                     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                         var activeTab = tabs[0];
                         chrome.tabs.sendMessage(activeTab.id, {"blockLink": currentUrl});
-                });
+                    });
+                }
             }
-        }
-        // });
+        });
     });
-        
-};
+}
 
 chrome.tabs.onActivated.addListener(handleTabChange);
 chrome.tabs.onUpdated.addListener(handleTabChange);
+
+// Add site to block list
+function addBlockSite(siteUrl) {
+    chrome.storage.sync.get(['blockList'], function(result) {
+        result.blockList.push(siteUrl)
+      chrome.storage.sync.set({'blockList': result.blockList}, function() {
+        console.log('Added ' + siteUrl + ' to block list');
+      });
+    });
+}
+
+addBlockSite("youtube.com");
