6a30122a6755a8d17344332a380e841ccfb99b0d
Arin Champati <champati.arin@gmail.com>
2021-01-27 18:50:54 -0500

    made icon logo have black background

--- content.js
+++ content.js
@@ -4,7 +4,9 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       // block webpage content
         if(request.blockLink) {
-            document.body.innerHTML = "";
+            `
+            
+            `;
         }
     }
   );
