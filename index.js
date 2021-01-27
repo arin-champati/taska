d43a66957dbd486b61017c82043c320d5bcece80
Jackson Deitelzweig <jackson.deitelzweig@gmail.com>
2021-01-27 15:27:17 -0500

    Oops forgot some js

---
+++ index.js
@@ -0,0 +1,15 @@
+  function start_working()
+  {
+  	var text = document.getElementById("working-button").value;
+  	if (text == "Start working!") {
+  		document.getElementById("working-button").value="Stop working!";
+  	}
+  	else {
+  		document.getElementById("working-button").value="Start working!";
+  	}
+  };
+
+  window.onload = function () {
+    let working_button = document.getElementById("working-button");
+    working_button.addEventListener("click", start_working);
+}
